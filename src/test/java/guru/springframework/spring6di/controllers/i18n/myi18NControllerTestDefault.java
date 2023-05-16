92d7eb187bede4135e6638ac6d90e6f06b28a479
Roman Ostrozovic <roman.ostrozovic@gmail.com>
2023-05-16 08:18:49 +0200

    Add "default " Profile for of two implementations of "i18NService" GreetingService for two languages EN, ES Add tests for this

---
+++ src/test/java/guru/springframework/spring6di/controllers/i18n/myi18NControllerTestDefault.java
@@ -0,0 +1,21 @@
+package guru.springframework.spring6di.controllers.i18n;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+//It will work, because we have "default" profile for "i18NService"
+//@ActiveProfiles("EN")
+@SpringBootTest
+public class myi18NControllerTestDefault {
+
+    @Autowired
+    Myi18NController myi18NController;
+
+    @Test
+    void sayHello() {
+        System.out.println(myi18NController.sayHello());
+    }
+
+}
